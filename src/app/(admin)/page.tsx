fca439bbd3a2eeb46a658b20de15391abbc9c0a1
anomalous254 <anomalous254@gmail.com>
2025-12-10 16:25:28 +0300

    removed analytics templates

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -9,9 +9,9 @@ import { ComingSoon } from "@/components/common/ComingSoon";
 
 export default function HomePage() {
   return (
-    <div className="grid grid-cols-12 gap-4 md:gap-6">
-      {/* <ComingSoon /> */}
-     <div className="col-span-12 space-y-6 xl:col-span-7">
+    <div /* className="grid grid-cols-12 gap-4 md:gap-6" */>
+      <ComingSoon />
+    {/*  <div className="col-span-12 space-y-6 xl:col-span-7">
 
         <NotificationMetrics />
 
@@ -24,7 +24,7 @@ export default function HomePage() {
       
       <div className="col-span-12">
          <StatisticsChart />
-      </div>
+      </div> */}
      
      
     </div>
