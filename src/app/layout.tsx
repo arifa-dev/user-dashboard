756f32185c26d7926fb71c5210bcb09953a2f045
anomalous254 <anomalous254@gmail.com>
2025-12-31 01:44:15 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { UserProvider } from '@/context/UserContext';
 import { WebSocketProvider } from '@/context/WebsocketContext';
+import MaintenancePage from './maintenance/page';
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -18,13 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${outfit.className} dark:bg-gray-900`}>
-        <ThemeProvider>
+        <MaintenancePage />
+       {/*  <ThemeProvider>
           <UserProvider>
            <WebSocketProvider>
              <SidebarProvider>{children}</SidebarProvider>
            </WebSocketProvider>
           </UserProvider>
-        </ThemeProvider>
+        </ThemeProvider> */}
       </body>
     </html>
   );
